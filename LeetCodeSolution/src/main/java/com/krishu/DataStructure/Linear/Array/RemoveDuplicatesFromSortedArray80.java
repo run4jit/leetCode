ef11e6e205cd981891bfff3323f818ff4f46b54a
Ranjeet <run4jit.it@gmail.com>
2026-07-16 01:19:43 +0530

    80. Remove Duplicates from Sorted Array II

---
+++ LeetCodeSolution/src/main/java/com/krishu/DataStructure/Linear/Array/RemoveDuplicatesFromSortedArray80.java
@@ -0,0 +1,117 @@
+package com.krishu.DataStructure.Linear.Array;
+
+/*
+80. Remove Duplicates from Sorted Array II
+
+
+Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+ */
+
+
+import com.krishu.Utility;
+
+public class RemoveDuplicatesFromSortedArray80 {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <=2 ) {
+            return nums.length;
+        }
+
+
+        int j = nums.length-1;
+        int last = nums.length-1;
+        /*
+        [1,1,1,2,2,3]
+        [0,0,1,1,1,1,2,3,3]
+        [0,0,1,1,1,1,2,3,3,3]
+         */
+        for (int i = nums.length-2; i >=0; i--) {
+            if (nums[i] == nums[j]) {
+                if ((j - i) > 1) {
+                    int k = i+1;
+                    while (k < last) {
+                        nums[k] = nums[++k];
+                    }
+                    j--;
+                    last--;
+                }
+            } else {
+                j--;
+            }
+        }
+
+
+        return last+1;
+
+    }
+}
+
+class Solution80 {
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+    public static void test1() {
+        int[] nums = {1,1,1,2,2,3};
+        int expected = 5;
+        int actual = RemoveDuplicatesFromSortedArray80.removeDuplicates(nums);
+        Utility.printException(expected, actual);
+    }
+
+    public static void test2() {
+        int[] nums = {0,0,1,1,1,1,2,3,3};
+        int expected = 7;
+        int actual = RemoveDuplicatesFromSortedArray80.removeDuplicates(nums);
+        Utility.printException(expected, actual);
+    }
+
+    public static void test3() {
+        int[] nums = {0,0,1,1,1,1,2,3,3,3};
+        int expected = 7;
+        int actual = RemoveDuplicatesFromSortedArray80.removeDuplicates(nums);
+        Utility.printException(expected, actual);
+    }
+}
